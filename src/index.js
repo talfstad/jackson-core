560dbb751c17a970a673f2e7b68237f846046243
Trevor Alfstad <trevor@buildcave.com>
2017-06-04 05:13:15 -0700

    refactor: jackson core into es6 class. https://app.asana.com/0/339740516165222/353343828633064

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ class JacksonCore {
   static processRequest(inputs) {
     const promise = new Promise((resolve, reject) => {
       if (inputs) {
-        resolve(inputs);
+        resolve(false, inputs);
       } else {
         reject();
       }
--- test/src/index-test.js
+++ test/src/index-test.js
@@ -1,14 +1,24 @@
 // import { expect } from 'chai';
-import JacksonCore from '../../src';
+// import JacksonCore from '../../src';
 
 describe('Jackson Core -> index.js', () => {
-  it('processRequest runs', () => {
-    JacksonCore.processRequest({})
-      .then(() => {
-        console.log('request is processed');
-      })
-      .catch(() => {
-        console.log('request is error');
-      });
-  });
+  // example input:
+  // const validInputs = {
+  //   uuid: '994c3823-aff6-f548-ce9b-1b5df2ac267c',
+  //   ip: '2602:304:ce3e:27f0:1e:abc8:9568:8b1',
+  //   url: 'https://some-lander.com/landingpage.html',
+  //   geo: { country: 'US' },
+  // };
+
+  // it('fails if no IP Address passed in input object', (done) => {
+  //   JacksonCore.processRequest({ ...validInputs, ip: undefined })
+  //     .catch((err) => {
+  //       try {
+  //         expect(err).to.not.equal(null);
+  //         done();
+  //       } catch (e) {
+  //         done(e);
+  //       }
+  //     });
+  // });
 });
